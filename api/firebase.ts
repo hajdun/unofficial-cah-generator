b09b445a8e319b351e27889fba58abbac7fba2cc
DESKTOP-VI6LSH9\NoraHajdu <hajdu.nora115@gmail.com>
2022-07-17 15:52:13 +0200

    Can add cards

--- api/firebase.ts
+++ api/firebase.ts
@@ -1,15 +1,15 @@
 import * as firebase from "firebase/app";
 import  { firebaseConfig } from "../firebaseConfig";
-import { getFirestore, collection, getDocs} from "firebase/firestore";
-import { doc, setDoc } from "firebase/firestore";
+import { getFirestore, collection, getDocs, doc, setDoc} from "firebase/firestore";
 
 export const app = firebase.initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const COLLECTION_NAME="cards"
+const cardsCol=collection(db, COLLECTION_NAME)
+
 
 // Get a list of cards from your database
 export const getCards = async () => {
-  const cardsCol = collection(db, COLLECTION_NAME);
   const cardSnapshot = await getDocs(cardsCol);
   const cardList = cardSnapshot.docs.map((doc) => doc.data());
   return cardList;
@@ -17,10 +17,12 @@ export const getCards = async () => {
 
  // Push Function
  export const addCard = (text: string, isQuestion: boolean, language="hu_HU", isFunny=true ) => {
-  setDoc(doc(db, 'unofficial-cah-generator', COLLECTION_NAME),{
+  setDoc(doc(cardsCol),{
         text,
         isQuestion,
         language,
         isFunny
+    }).then(()=>{
+      return getCards()
     })
   }
