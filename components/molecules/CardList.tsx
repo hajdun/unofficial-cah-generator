f0e5a2b9165a23babea5edcde6b1d23946161c5c
DESKTOP-VI6LSH9\NoraHajdu <hajdu.nora115@gmail.com>
2022-07-17 21:49:12 +0200

    Add cardlist operations

--- components/molecules/CardList.tsx
+++ components/molecules/CardList.tsx
@@ -7,18 +7,70 @@ import styles from "./CardList.module.css"
 
 const CardList: React.FC = () => {
     const [cards, setCards] = useState<ICard[]>([])
+    const [isUnfunnyHidden, setIsUnfunnyHidden] = useState(false)
 
     useEffect(() => {
-        getCards().then((cardList) => {
-            setCards(cardList);
-        });
+        if (!cards || cards.length <= 0)
+            getCards().then((cardList) => {
+                setCards(cardList);
+            });
     }, []);
 
+    const sortOutQuestionsAnswers = () => {
+        if (!cards || cards.length <= 0) return
+        let questions = []
+        let answers = []
+        for (let i = 0; i < cards.length; i++) {
+            const card = cards[i]
+            if (card.isQuestion === "true") {
+                questions.push(card)
+            } else {
+                answers.push(card)
+            }
+        }
+        const separatedArray = [...questions, ...answers] || cards
+        setCards(separatedArray)
+    }
+
+    const compare = (a: ICard, b: ICard) => {
+        if (a.text < b.text) {
+            return -1;
+        }
+        if (a.text > b.text) {
+            return 1;
+        }
+        return 0;
+    }
+
+    const sortAlpha = () => {
+        if (!cards || cards.length <= 0) return
+        const sortedArray = cards.sort(compare) || cards;
+        setCards([...sortedArray])
+    }
+
+const hideUnfunny=()=>{
+    setIsUnfunnyHidden(true)
+}
+
     return (
-        <div className={styles.cardList}>
-            {cards.map(card => {
-                return <Card key={uuidv4()} card={card}/>
-            })}
+        <div>
+            <div className={styles.cardListActions}>
+                <div>
+                    <button className={styles.button} onClick={sortOutQuestionsAnswers}>Separate questions from answers</button>
+                </div>
+                <div>
+                    <button className={styles.button} onClick={sortAlpha}>Sort alphabetically</button>
+                </div>
+                <div>
+                    <button className={styles.button} onClick={hideUnfunny}>Hide unfunny</button>
+                </div>
+            </div>
+            <div className={styles.cardList}>
+                {cards.map(card => {
+if(isUnfunnyHidden && card.isFunny===false) return ""
+                    return <Card key={uuidv4()} card={card} />
+                })}
+            </div>
         </div>
     );
 };
